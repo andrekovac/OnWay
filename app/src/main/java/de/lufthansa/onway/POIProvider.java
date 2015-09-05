db6c7597877459d09b156c70354519f9b5a45258
Andre Kovac <andrekovac@t-online.de>
2015-09-05 11:50:07 +0200

    edited icons and names

--- app/src/main/java/de/lufthansa/onway/POIProvider.java
+++ app/src/main/java/de/lufthansa/onway/POIProvider.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class POIProvider {
 
     List<DistanceAwarePOI> poisOnWay = new ArrayList<DistanceAwarePOI>() {{
-        add(new DistanceAwarePOI("Bus-Stop", 5, R.drawable.ic_action_bus,"E9926BABC410"));
-        add(new DistanceAwarePOI("Coffee", 10, R.drawable.ic_action_coffee2go,"C477D9A011D2"));
-        add(new DistanceAwarePOI("Duty free", 10, R.drawable.ic_action_shopping_cart,"E21A05D73C02"));
-        add(new DistanceAwarePOI("Duty free", 10, R.drawable.ic_action_shopping_cart,"C03DC1DD04A5"));
+        add(new DistanceAwarePOI("Entrance", 5, R.drawable.ic_action_bus,"E9926BABC410"));
+        add(new DistanceAwarePOI("Duty Free", 10, R.drawable.ic_action_shopping_cart,"C477D9A011D2"));
+        add(new DistanceAwarePOI("Charging Point", 10, R.drawable.ic_action_plug,"E21A05D73C02"));
+        add(new DistanceAwarePOI("Toilet", 10, R.drawable.ic_action_wc,"C03DC1DD04A5"));
         add(new DistanceAwarePOI("Coffee", 10, R.drawable.ic_action_coffee2go,"CF34DF538243"));
     }};
 
